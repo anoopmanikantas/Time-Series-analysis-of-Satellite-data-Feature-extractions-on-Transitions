229fad40fbd4c7cb3803d288252156e6a37ae981
sbh69840 <sbh69840@gmail.com>
2021-07-18 12:26:00 +0530

    modified requirements.txt file

--- run.sh
+++ run.sh
@@ -2,3 +2,10 @@
 python3 -m venv satpro1
 source satpro1/bin/activate
 pip install -r requirements.txt
+pip install jupyter_http_over_ws
+jupyter serverextension enable --py jupyter_http_over_ws
+jupyter notebook \
+  --NotebookApp.allow_origin='https://colab.research.google.com' \
+  --port=8888 \
+  --NotebookApp.port_retries=0
+
